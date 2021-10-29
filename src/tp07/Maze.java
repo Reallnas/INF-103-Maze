e1b19f42144310552ad56f78c551549cb214ed6d
Minier Arnaud <arnaud.minier@telecom-paris.fr>
2021-10-29 10:21:09 +0200

    Add Maze class

---
+++ src/tp07/Maze.java
@@ -0,0 +1,12 @@
+package tp07;
+
+import java.util.ArrayList;
+
+public class Maze {
+
+    public ArrayList<ArrayList<MBox>> grid;
+    
+    public Maze(ArrayList<ArrayList<MBox>> grid) {
+        this.grid = grid;
+    }
+}
