6a14a027b9fe47cd2b80b09adc10303e297e6219
Minier Arnaud <arnaud.minier@telecom-paris.fr>
2021-11-26 10:38:18 +0100

    MazeReadingException: add custom printing

--- src/tp07/MazeReadingException.java
+++ src/tp07/MazeReadingException.java
@@ -2,15 +2,21 @@ package tp07;
 
 public class MazeReadingException extends Exception {
 
+    private final String message;
     private final String fileName;
-    private final int errorLine;
+    private final int line;
     
-    public MazeReadingException(String message,String fileName, int errorLine) {
-        super(message);
+    public MazeReadingException(String message,String fileName, int line) {
+        super();
+        this.message = message;
         this.fileName = fileName;
-        this.errorLine = errorLine;
+        this.line = line;
+    }
+    
+    @Override
+    public final String toString()
+    {
+        return String.format("%s, line %d: %s",fileName,line,message);
     }
 
-    
-    
 }
