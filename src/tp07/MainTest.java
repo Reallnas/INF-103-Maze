ebf7bc9a490d2b017cc1204d3a3ef96360de7e52
Minier Arnaud <arnaud.minier@telecom-paris.fr>
2021-11-26 09:44:14 +0100

    MainTest: create tests for the Maze class

---
+++ src/tp07/MainTest.java
@@ -0,0 +1,10 @@
+package tp07;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        Maze m = new Maze();
+        m.initFromTextFile("data/labyrinthe.txt");
+    }
+
+}
