73ea9a3cd757b54d892d82971310bb3e8b139c8a
Minier Arnaud <arnaud.minier@telecom-paris.fr>
2021-12-23 18:24:08 +0100

    MazeModel: Add the necessary functions to select a box

--- src/model/MazeModel.java
+++ src/model/MazeModel.java
@@ -1,6 +1,5 @@
 package model;
 
-import maze.MBox;
 import maze.Maze;
 
 import java.awt.*;
@@ -10,6 +9,7 @@ public class MazeModel {
 
     private final Maze maze = new Maze();
     private final ArrayList<ArrayList<MazeBox>> boxes;
+    private MazeBox selectedBox = null;
     private boolean modified = false;
     //The number of box in a line
     private int nb_box_x = 10;
@@ -22,8 +22,8 @@ public class MazeModel {
         boxes = new ArrayList<>();
         for (int i = 0; i < nb_box_x; i++) {
             boxes.add(new ArrayList<>());
-            for(int j = 0; j < nb_box_y; j++) {
-                boxes.get(i).add(new MazeBox(i*boxWidth,j*boxHeight,boxWidth,boxHeight,Color.BLACK));
+            for (int j = 0; j < nb_box_y; j++) {
+                boxes.get(i).add(new MazeBox(i * boxWidth, j * boxHeight, boxWidth, boxHeight, Color.BLACK));
             }
         }
     }
@@ -33,22 +33,52 @@ public class MazeModel {
     }
 
     public void notifyWindowSizeChange(float width, float height) {
-        boxWidth = width/nb_box_x;
-        boxHeight = height/nb_box_y;
+        boxWidth = width / nb_box_x;
+        boxHeight = height / nb_box_y;
         for (int x = 0; x < nb_box_x; x++) {
-            for(int y = 0; y < nb_box_y; y++) {
-                boxes.get(x).get(y).setRect(x*boxWidth,y*boxHeight,boxWidth,boxHeight);
+            for (int y = 0; y < nb_box_y; y++) {
+                boxes.get(x).get(y).setRect(x * boxWidth, y * boxHeight, boxWidth, boxHeight);
             }
         }
     }
 
-    public final void paintBoxes(Graphics g)
-    {
-        for(ArrayList<MazeBox> column: boxes) {
-            for(MazeBox box : column) {
-                box.paint(g,false);
+    public final void paintBoxes(Graphics g) {
+        for (ArrayList<MazeBox> column : boxes) {
+            for (MazeBox box : column) {
+                box.paint(g, false);
             }
         }
+    }
 
+    public final MazeBox getSelectedBox() {
+        return selectedBox;
+    }
+
+    public final void setSelectedSBox(MazeBox selectedBox) {
+        if (this.selectedBox != selectedBox) {
+            this.selectedBox = selectedBox;
+            modified = true;
+            stateChanges();
+        }
+    }
+
+    public final void setSelection(int x, int y) {
+        for (ArrayList<MazeBox> col : boxes) {
+            for (MazeBox box : col) {
+                if (box.contains(x, y)) {
+                    setSelectedSBox(box);
+                    System.out.println(box);
+                    return;
+                }
+            }
+        }
+        setSelectedSBox(null);
+    }
+
+    public void stateChanges() {
+        /*ChangeEvent evt = new ChangeEvent(this) ;
+        for (ChangeListener listener : listeners) {
+            listener.stateChanged(evt);
+        }*/
     }
 }
