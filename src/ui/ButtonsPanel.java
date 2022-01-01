fd190447cad35acb1532ed78dafc27f6a903a365
Minier Arnaud <arnaud.minier@telecom-paris.fr>
2022-01-01 17:20:06 +0100

    Buttons : Remove the "Save" and "Load" buttons

--- src/ui/ButtonsPanel.java
+++ src/ui/ButtonsPanel.java
@@ -8,18 +8,14 @@ public class ButtonsPanel extends JPanel {
     private final ChangeBoxTypeButton changeBoxTypeButton;
     private final SetAsStartButton setAsStartButton;
     private final SetAsGoalButton setAsGoalButton;
-    private final LoadButton loadButton;
-    private final SaveButton saveButton;
     private final FindPathButton findPathButton;
 
     public ButtonsPanel(MainWindow mainWindow) {
-        setLayout(new GridLayout(2, 3));
+        setLayout(new GridLayout(2, 2));
 
         add(changeBoxTypeButton = new ChangeBoxTypeButton(mainWindow));
         add(setAsStartButton = new SetAsStartButton(mainWindow));
         add(setAsGoalButton = new SetAsGoalButton(mainWindow));
-        add(loadButton = new LoadButton(mainWindow));
-        add(saveButton = new SaveButton(mainWindow));
         add(findPathButton = new FindPathButton(mainWindow));
     }
 
@@ -27,7 +23,6 @@ public class ButtonsPanel extends JPanel {
         changeBoxTypeButton.notifyForUpdate();
         setAsStartButton.notifyForUpdate();
         setAsGoalButton.notifyForUpdate();
-        saveButton.notifyForUpdate();
         findPathButton.notifyForUpdate();
     }
 }
