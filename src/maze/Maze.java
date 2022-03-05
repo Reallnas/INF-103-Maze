5a9e0005c23ea4bdbe3786be8b3c22cdc4856784
Minier Arnaud <arnaud.minier@telecom-paris.fr>
2022-03-05 18:07:49 +0100

    Maze: Update the documentation

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -16,14 +16,29 @@ public class Maze implements GraphInterface {
     private int width = 0;
     private int height = 0;
 
+    /**
+     * Get the width of the maze.
+     *
+     * @return The width of the maze.
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * Get the height of the maze.
+     *
+     * @return The height of the maze.
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * Get the root of the maze.
+     *
+     * @return A reference to the root of the maze if there is one defined; null otherwise.
+     */
     public VertexInterface getRoot() {
         return this.root;
     }
@@ -35,7 +50,6 @@ public class Maze implements GraphInterface {
         return srcBox.isWalkable() && dstBox.isWalkable() && srcBox.isNeighborWith(dstBox);
     }
 
-    //Return the number of boxes in the maze
     @Override
     public int getSize() {
         return width * height;
@@ -50,7 +64,12 @@ public class Maze implements GraphInterface {
         return vertices;
     }
 
-    //Returns an ArrayList of all the empty boxes that are connected to the MBox passed in argument
+    /**
+     * Get the list of all the empty boxes that are connected to the MBox passed in argument
+     *
+     * @param vertex the box to list the neighbors of
+     * @return The list of all the empty boxes that are connected to the specified MBox
+     */
     @Override
     public ArrayList<VertexInterface> getSuccessors(VertexInterface vertex) {
         MBox vBox = (MBox) vertex;
@@ -95,6 +114,13 @@ public class Maze implements GraphInterface {
             return 1;
     }
 
+    /**
+     * Get a set of connected vertices that form a path between the start and the goal.
+     *
+     * @param p The connections between the vertices (often the result of the Dijkstra function).
+     * @return A set of connected vertices that form a path between the start and the goal.
+     * Warning: the path may be incomplete if it is not possible to join the start and the goal.
+     */
     public ASetInterface getPathToGoal(PreviousInterface p) {
         ASet path = new ASet();
         VertexInterface current = this.goal;
@@ -107,10 +133,21 @@ public class Maze implements GraphInterface {
         return path;
     }
 
+    /**
+     * Get a set of connected vertices that form a path between the start and the goal.
+     *
+     * @return A set of connected vertices that form a path between the start and the goal if there is one;
+     * an empty set otherwise.
+     */
     public ASetInterface getPathToGoal() {
         return getPathToGoal(Dijkstra.dijkstra(this, root));
     }
 
+    /**
+     * Initialize the maze with the data found in the specified file.
+     *
+     * @param fileName The name of the file that contains the maze data.
+     */
     public final void initFromTextFile(String fileName) {
         int newWidth = 0;
         int newHeight = 0;
@@ -164,6 +201,12 @@ public class Maze implements GraphInterface {
         System.out.printf("New Maze Height : %d%n", height);
     }
 
+    /**
+     * Save the maze to the specified file.
+     *
+     * @param fileName The name of the file where the data will be saved.
+     * @return true if the maze has been successfully saved; false otherwise.
+     */
     public final boolean saveToTextFile(String fileName) {
         try (FileOutputStream fos = new FileOutputStream(fileName);
              PrintWriter pw = new PrintWriter(fos)) {
@@ -183,6 +226,11 @@ public class Maze implements GraphInterface {
         return true;
     }
 
+    /**
+     * Print the maze in the console with the specified path.
+     *
+     * @param path the vertices that form a path.
+     */
     public final void printWithPath(ASetInterface path) {
         for (int y = 0; y < this.height; y++) {
             for (int x = 0; x < this.width; x++) {
@@ -196,10 +244,21 @@ public class Maze implements GraphInterface {
         }
     }
 
+    /**
+     * Print the maze in the console with the path between the start and the goal.
+     *
+     * @param p The connections between the vertices (often the result of the Dijkstra function).
+     */
     public final void printWithPath(PreviousInterface p) {
         this.printWithPath(this.getPathToGoal(p));
     }
 
+    /**
+     * Initialize an empty maze with the specified width and height.
+     *
+     * @param width  The new width of the maze.
+     * @param height The new height of the maze.
+     */
     public void initializeEmptyMaze(int width, int height) {
         goal = null;
         root = null;
@@ -214,6 +273,10 @@ public class Maze implements GraphInterface {
         }
     }
 
+    /**
+     * Remove the start of the maze if there is one and replace it with an empty box.
+     * If there is none, nothing happens.
+     */
     private void removeStart() {
         if (root != null) {
             int oldRootX = root.getX();
@@ -222,6 +285,10 @@ public class Maze implements GraphInterface {
         }
     }
 
+    /**
+     * Remove the goal of the maze if there is one and replace it with an empty box.
+     * If there is none, nothing happens.
+     */
     private void removeGoal() {
         if (goal != null) {
             int oldGoalX = goal.getX();
@@ -230,6 +297,14 @@ public class Maze implements GraphInterface {
         }
     }
 
+    /**
+     * Set the start of the maze at the given coordinates.
+     * If the start was already defined, it replaces the old one with an empty box.
+     * If the new start is at the same coordinates as the current goal, it removes the current goal.
+     *
+     * @param x The X coordinates of the start.
+     * @param y The Y coordinates of the start.
+     */
     public void setNewStart(int x, int y) {
         System.out.printf("New Start %d %d%n", x, y);
         //If we overwrite the current goal, we need to update the reference to avoid referencing an outdated Box
@@ -242,6 +317,14 @@ public class Maze implements GraphInterface {
         this.boxGrid.get(x).set(y, this.root);
     }
 
+    /**
+     * Set the goal of the maze at the given coordinates.
+     * If the goal was already defined, it replaces the old one with an empty box.
+     * If the new goal is at the same coordinates as the current start, it removes the current start.
+     *
+     * @param x The X coordinates of the goal.
+     * @param y The Y coordinates of the goal.
+     */
     public void setNewGoal(int x, int y) {
         System.out.printf("New Goal %d %d%n", x, y);
         //If we overwrite the current root, we need to update the reference to avoid referencing an outdated Box
@@ -254,6 +337,14 @@ public class Maze implements GraphInterface {
         this.boxGrid.get(x).set(y, this.goal);
     }
 
+    /**
+     * Change the box type at the specified coordinates.
+     * If it was an empty box, the start or the goal, it will be changed to a wall.
+     * If it was a wall, it will be changed to an empty box.
+     *
+     * @param x The X coordinates of the box.
+     * @param y The Y coordinates of the box.
+     */
     public void changeBoxType(int x, int y) {
         //If we overwrite the current root or goal, we need to update the reference to avoid referencing an outdated Box
         if (goal != null && goal.getX() == x && goal.getY() == y) {
@@ -271,10 +362,22 @@ public class Maze implements GraphInterface {
         }
     }
 
+    /**
+     * Check if it is theoretically possible to find a path between the start and the goal (ie they are both set).
+     *
+     * @return true if the start and the goal are set; false otherwise.
+     */
     public boolean canFindAPath() {
         return root != null && goal != null;
     }
 
+    /**
+     * Get the box at the specified coordinates.
+     *
+     * @param x The X coordinates of the box.
+     * @param y The Y coordinates of the box.
+     * @return The box at the specified coordinates
+     */
     public MBox getMBoxByCoords(int x, int y) {
         return boxGrid.get(x).get(y);
     }
